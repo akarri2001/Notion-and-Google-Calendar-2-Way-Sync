8c997cd36b2520dd47e2324a16aee93f4127ece6
kat <28567881+hk21702@users.noreply.github.com>
2022-08-15 22:07:23 -0400

    use local server for google auth

--- GCalToken.py
+++ GCalToken.py
@@ -8,7 +8,7 @@ from datetime import datetime, timedelta, date
 
 scopes = ['https://www.googleapis.com/auth/calendar']
 flow = InstalledAppFlow.from_client_secrets_file("client_secret.json", scopes=scopes)
-credentials = flow.run_console()
+credentials = flow.run_local_server()
 pickle.dump(credentials, open("token.pkl", "wb"))
 credentials = pickle.load(open("token.pkl", "rb"))
 service = build("calendar", "v3", credentials=credentials)
